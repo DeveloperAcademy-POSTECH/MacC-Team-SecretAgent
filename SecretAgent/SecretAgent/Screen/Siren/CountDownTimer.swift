d8c916dbebe029a4c90a8e7c820473022113b1aa
Miller <eotjd8721@naver.com>
2022-11-19 20:28:50 +0900

    [FEAT] 타이머 기능 구현

---
+++ SecretAgent/SecretAgent/Screen/Siren/CountDownTimer.swift
@@ -0,0 +1,84 @@
+//
+//  CountDownTimer.swift
+//  SecretAgent
+//
+//  Created by DaeSeong on 2022/11/19.
+//
+
+import UIKit
+
+protocol CountdownTimerDelegate: AnyObject {
+    func countdownTimerDone()
+    func countdownTime(time: (minutes: String, seconds: String))
+}
+
+final class CountDownTimer {
+    // MARK: - Properties
+
+    weak var delegate: CountdownTimerDelegate?
+
+    private var seconds = 0.0
+    var duration = 0.0
+
+    private var timer: Timer = .init()
+
+    // MARK: - Func
+
+    // 타이머 설정
+    func setTimer(minutes: Int, seconds: Int) {
+        let minutesToSeconds = minutes * 60
+        let secondsToSeconds = seconds
+
+        let seconds = secondsToSeconds + minutesToSeconds
+        self.seconds = Double(seconds)
+        duration = Double(seconds)
+
+        delegate?.countdownTime(time: timeString(time: TimeInterval(ceil(duration))))
+    }
+
+    // 타이머 실행
+    private func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true) // 왜 0.01초로 했을까?
+    }
+
+    // 타이머 실시간 업데이트
+    @objc private func updateTimer() {
+        if duration <= 0.0 {
+            timerDone()
+        } else {
+            duration -= 0.01
+            delegate?.countdownTime(time: timeString(time: TimeInterval(ceil(duration))))
+        }
+    }
+
+    // 타이머 종료
+    private func timerDone() {
+        timer.invalidate()
+        duration = seconds
+        delegate?.countdownTimerDone()
+    }
+
+    // 타이머 실행 버튼 클릭시
+    func start() {
+        runTimer()
+    }
+
+    // 타이머 일시정지 버튼 클릭시
+    @objc func pause() {
+        timer.invalidate()
+    }
+
+    // 타이머 멈춤 버튼 클릭시
+    func stop() {
+        timer.invalidate()
+        duration = seconds
+        delegate?.countdownTime(time: timeString(time: TimeInterval(ceil(duration))))
+    }
+
+    // TimeIntervalToString
+    private func timeString(time: TimeInterval) -> (minutes: String, seconds: String) {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return (minutes: String(format: "%02d", minutes), seconds: String(format: "%02d", seconds))
+    }
+}
---
+++ SecretAgent/SecretAgent/Screen/Siren/SirenViewController.swift
@@ -0,0 +1,129 @@
+//
+//  SirenViewController.swift
+//  SecretAgent
+//
+//  Created by DaeSeong on 2022/11/19.
+//
+
+import SnapKit
+import UIKit
+
+final class SirenViewController: BaseViewController {
+    // MARK: - Properties
+
+    @IBOutlet var minutes: UILabel!
+
+    @IBOutlet var seconds: UILabel!
+
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var stopButton: UIButton!
+    @IBOutlet var progressBar: ProgressBar!
+    @IBOutlet var counterView: UIStackView!
+
+    private let countdownTimer: CountDownTimer = .init()
+
+    private let doneLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24.0,
+                                       weight: UIFont.Weight.light)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        label.text = "Done!"
+        return label
+    }()
+
+    private var countdownTimerDidStart = false
+    private let selectedSecs: Int = 30 * 60
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDelegation()
+        setNotification()
+    }
+
+    override func render() {
+        view.addSubview(doneLabel)
+        doneLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+    }
+
+    override func configUI() {
+        super.configUI()
+        countdownTimer.setTimer(minutes: 0, seconds: selectedSecs)
+        progressBar.setProgressBar(minutes: 0, seconds: selectedSecs)
+        stopButton.isEnabled = false
+        stopButton.alpha = 0.5
+        doneLabel.isHidden = true
+        counterView.isHidden = false
+
+        minutes.text = String(format: "%02d", selectedSecs / 60 % 60)
+        seconds.text = String(format: "%02d", selectedSecs % 60)
+    }
+
+    // MARK: - Func
+
+    private func setDelegation() {
+        countdownTimer.delegate = self
+    }
+
+    private func setNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(addBackgroundTime(_:)), name: NSNotification.Name("sceneWillEnterForeground"), object: nil)
+    }
+
+    @objc func addBackgroundTime(_ notification: Notification) {
+        if countdownTimerDidStart == true {
+            let time = notification.userInfo?["time"] as? Int ?? 0
+            countdownTimer.duration -= Double(time)
+        }
+    }
+
+    @IBAction func stopTimer(_ sender: UIButton) {
+        countdownTimer.stop()
+        progressBar.stop()
+        countdownTimerDidStart = false
+        stopButton.isEnabled = false
+        stopButton.alpha = 0.5
+        startButton.setTitle("START", for: .normal)
+    }
+
+    @IBAction func startTimer(_ sender: UIButton) {
+        doneLabel.isHidden = true
+        counterView.isHidden = false
+        stopButton.isEnabled = true
+        stopButton.alpha = 1.0
+
+        if !countdownTimerDidStart {
+            countdownTimer.start()
+            progressBar.start()
+            countdownTimerDidStart = true
+            startButton.setTitle("PAUSE", for: .normal)
+        } else {
+            countdownTimer.pause()
+            progressBar.pause()
+            countdownTimerDidStart = false
+            startButton.setTitle("RESUME", for: .normal)
+        }
+    }
+}
+
+// MARK: CountdownTimerDelegate
+
+extension SirenViewController: CountdownTimerDelegate {
+    func countdownTime(time: (minutes: String, seconds: String)) {
+        minutes.text = time.minutes
+        seconds.text = time.seconds
+    }
+
+    func countdownTimerDone() {
+        doneLabel.isHidden = false
+        counterView.isHidden = true
+        stopButton.isEnabled = false
+        stopButton.alpha = 0.5
+        startButton.setTitle("START", for: .normal)
+        countdownTimerDidStart = false
+    }
+}
