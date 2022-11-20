714517683f53481ab65a0b6329d8e171e9c3a1a5
Miller <eotjd8721@naver.com>
2022-11-20 23:04:48 +0900

    [CHORE] selectedSeconds 상수값 변경

--- SecretAgent/SecretAgent/Screen/Siren/SirenViewController.swift
+++ SecretAgent/SecretAgent/Screen/Siren/SirenViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 private enum Constants {
     static let progressBarSize = 300
-    static let selectedSeconds = 10
+    static let selectedSeconds = 30 * 60
 }
 
 final class SirenViewController: BaseViewController {
