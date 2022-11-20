54667478d9f5b989887f76cdde64e1bda42626a3
Miller <eotjd8721@naver.com>
2022-11-20 22:49:50 +0900

    [FIX] feedback resolved third

--- SecretAgent/SecretAgent/Screen/Siren/CountDownTimer.swift
+++ SecretAgent/SecretAgent/Screen/Siren/CountDownTimer.swift
@@ -17,25 +17,28 @@ final class CountDownTimer {
 
     weak var delegate: CountdownTimerDelegate?
 
-    private var seconds = 0.0
-    var duration = 0.0
+    private let totalSeconds: Int
+    var duration: Double = 0.0
 
     private var timer: Timer = .init()
+
+    // MARK: - Init
+
+    init(totalSeconds: Int) {
+        self.totalSeconds = totalSeconds
+        duration = Double(totalSeconds)
+    }
 
     // MARK: - Func
 
     // 타이머 설정
-    func setTimer(minutes: Int, seconds: Int) {
-        let totalSeconds = minutes * 60 + seconds
-        self.seconds = Double(totalSeconds)
-        duration = Double(totalSeconds)
-
+    func setTimer() {
         delegate?.countdownTime(time: timeString(time: TimeInterval(ceil(duration))))
     }
 
     // 타이머 실행
     func runTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true) // 왜 0.01초로 했을까?
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
 
     // 타이머 실시간 업데이트
@@ -51,25 +54,25 @@ final class CountDownTimer {
     // 타이머 종료
     private func timerDone() {
         timer.invalidate()
-        duration = seconds
+        duration = Double(totalSeconds)
         delegate?.countdownTimerDone()
     }
 
     // 타이머 일시정지 버튼 클릭시
-    @objc func pause() {
+    func pause() {
         timer.invalidate()
     }
 
     // 타이머 멈춤 버튼 클릭시
     func stop() {
         timer.invalidate()
-        duration = seconds
+        duration = Double(totalSeconds)
         delegate?.countdownTime(time: timeString(time: TimeInterval(ceil(duration))))
     }
 
     // TimeIntervalToString
     private func timeString(time: TimeInterval) -> (minutes: String, seconds: String) {
-        let minutes = Int(time) / 60 % 60
+        let minutes = Int(time) / 60
         let seconds = Int(time) % 60
         return (minutes: String(format: "%02d", minutes), seconds: String(format: "%02d", seconds))
     }
