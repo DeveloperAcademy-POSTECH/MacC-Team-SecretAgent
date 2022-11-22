90ccb3f0c1b350889a4d4ecd7cd0766edfe24f98
Minkyeong-Ko <artngmin01@gmail.com>
2022-11-22 14:19:16 +0900

    [FEAT] 사이렌 재생 코드 추가

---
+++ SecretAgent/SecretAgent/Screen/Siren/TestSirenViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TestSirenViewController.swift
+//  SecretAgent
+//
+//  Created by Minkyeong Ko on 2022/11/21.
+//
+
+import AVFoundation
+import UIKit
+
+import SnapKit
+
+class TestSirenViewController: BaseViewController, AVAudioPlayerDelegate {
+    // MARK: - Properties
+
+    private var sirenPlayer: AVAudioPlayer?
+
+    private lazy var sirenButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("사이렌 울리기", for: .normal)
+        button.addTarget(self, action: #selector(playSiren), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func render() {
+        view.addSubview(sirenButton)
+        sirenButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+
+    @objc func playSiren() {
+        guard let path = Bundle.main.path(forResource: "siren.mp3", ofType: nil) else { return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            sirenPlayer = try AVAudioPlayer(contentsOf: url)
+            sirenPlayer?.delegate = self
+            sirenPlayer?.play()
+        } catch {
+            print(error)
+        }
+    }
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print(flag)
+        // TODO: 타이머 화면 시작
+    }
+}
