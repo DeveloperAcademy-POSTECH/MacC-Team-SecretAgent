4c04c415f9704d1ae9d6b86564beb8b5e3211773
Taekyun Kim <taekkim@student.42seoul.kr>
2022-11-25 18:11:20 +0900

    [ADD]: Image Assets #27

--- SecretAgent/SecretAgent/Global/Literal/ImageLiteral.swift
+++ SecretAgent/SecretAgent/Global/Literal/ImageLiteral.swift
@@ -9,8 +9,24 @@ import Foundation
 import UIKit
 
 enum ImageLiteral {
-    // 예시
-    static var sampleRoom: UIImage { .load(named: "SampleRoom") }
+    // MARK: - Badges
+    static var coin: UIImage { .load(named: "coin.activate") }
+    static var shield: UIImage { .load(named: "shield.activate") }
+    static var star: UIImage { .load(named: "star.activate") }
+    static var deactivateCoin: UIImage { .load(named: "coin.deactivate") }
+    static var deactivateShield: UIImage { .load(named: "shield.deactivate") }
+    static var deactivateStar: UIImage { .load(named: "star.deactivate") }
+
+    // MARK: - TabBar
+    static var story: UIImage { .load(named: "story.activate") }
+    static var deactivateStory: UIImage { .load(named: "story.deactivate") }
+
+    // MARK: - NavigationBarButton
+    static var agent: UIImage { .load(named: "agent") }
+
+    // MARK: - Siren
+    static var siren: UIImage { .load(named: "siren") }
+    static var woongwoong: UIImage { .load(named: "woongwoong") }
 }
 
 extension UIImage {
