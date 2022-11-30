131a703090e3aa2e7adcbe874125959927e61813
Miller <eotjd8721@naver.com>
2022-11-30 18:33:05 +0900

    [FEAT] 사운드매니저 구현

---
+++ SecretAgent/SecretAgent/Global/Service/SoundManager.swift
@@ -0,0 +1,45 @@
+//
+//  SoundManager.swift
+//  SecretAgent
+//
+//  Created by DaeSeong on 2022/11/30.
+//
+
+import AVFoundation
+import UIKit
+
+class SoundManager {
+    // MARK: - Properties
+
+    static let shared = SoundManager()
+    private var player: AVAudioPlayer?
+
+    // MARK: - Init
+
+    private init() {}
+
+    // MARK: - Func
+
+    func setupSound(soundOption: SoundLiteral, repeated: Bool) {
+        guard let url = Bundle.main.url(forResource: soundOption(), withExtension: MusicExtension.mp3()) else { return }
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let sound = player else { return }
+            if repeated {
+                sound.numberOfLoops = -1
+            } else {
+                sound.numberOfLoops = 0
+            }
+            sound.prepareToPlay()
+        } catch {
+            print("오디오 파일을 찾을 수 없습니다.")
+        }
+    }
+
+    func playSound() {
+        if let player {
+            player.play()
+        }
+    }
+}
