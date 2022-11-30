3588ae83ac1aef81d10589e165ec2814db4cd373
Miller <eotjd8721@naver.com>
2022-12-01 01:03:42 +0900

    [CHORE] 디테일 수정

--- SecretAgent/SecretAgent/Screen/Siren/SirenViewController.swift
+++ SecretAgent/SecretAgent/Screen/Siren/SirenViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 private enum Constants {
     static let progressBarSize = 300
-    static let selectedSeconds = 10 * 1 // 임시
+    static let selectedSeconds = 5 * 1 // 임시
     static let timeHStackViewHorizontalOffset = UIScreen.main.bounds.width / 2.48
     static let buttonHStackViewBottomOffset = UIScreen.main.bounds.height / 5.20
     static let defaultOffset = 20
