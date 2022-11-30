333f4dbc19244f3baaf31c77f2d30318375659d2
Miller <eotjd8721@naver.com>
2022-12-01 00:48:49 +0900

    [FEAT] 사이렌 알림 구현

--- SecretAgent/SecretAgent/Global/Service/UserNotificationService.swift
+++ SecretAgent/SecretAgent/Global/Service/UserNotificationService.swift
@@ -11,6 +11,10 @@ import UserNotifications
 struct UserNotificationManager {
     private let center = UNUserNotificationCenter.current()
 
+    static let shared = UserNotificationManager()
+
+    private init() {}
+
     func grant(of options: UNAuthorizationOptions = [.alert, .sound, .badge]) {
         center.getNotificationSettings { setting in
             guard setting.authorizationStatus == .notDetermined else { return }
