9c3f156076747cf3df6822521322d78c05333d24
JIW00NG <ipkjw2@gmail.com>
2022-12-01 12:19:22 +0900

    Revert "Revert "Merge branch 'dev' into 35-onboarding-view""
    This reverts commit df11d67be9a5000a10d2f944ab35213ae1ec81d5.

---
+++ SecretAgent/SecretAgent/Screen/Siren/View/Cell/NumberCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  NumberCollectionViewCell.swift
+//  SecretAgent
+//
+//  Created by DaeSeong on 2022/11/28.
+//
+
+import UIKit
+
+import SnapKit
+
+class NumberCollectionViewCell: UICollectionViewCell {
+    // MARK: - Properties
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        return label
+    }()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+        configUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Func
+
+    private func render() {
+        contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+
+    private func configUI() {
+        contentView.backgroundColor = .yoGray2
+        contentView.layer.cornerRadius = 10
+    }
+
+    func configure(with number: Int) {
+        titleLabel.text = String(number % 10)
+    }
+}
