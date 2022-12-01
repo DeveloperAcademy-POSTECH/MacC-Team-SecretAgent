13d136325603c98c1f1f7d6bdd8f817939f2854e
Minkyeong-Ko <artngmin01@gmail.com>
2022-12-01 21:38:25 +0900

    [FIX] #42 : QA에서 나온 여러 버그 수정, 임무 완료 멘트 추가

--- SecretAgent/SecretAgent/Screen/Board/View/BoardViewController.swift
+++ SecretAgent/SecretAgent/Screen/Board/View/BoardViewController.swift
@@ -19,19 +19,25 @@ enum BoardSize {
     static let coinSize = CGSize(width: 90, height: 96.15)
     static let shieldSize = CGSize(width: 96, height: 135.17)
     static let starSize = CGSize(width: 163.0, height: 196.0)
+    static let mentSize = CGSize(width: 170, height: 87)
 }
 
 final class BoardViewController: BaseViewController {
     // MARK: - Properties
 
+    // 뱃지 관련
     var totalBadgeNumber: Int = 0
     var totalBadgeNumberFromCoreData: Int = 0
+    var allStarsCollected = false
+
+    // 테이블 뷰 관련
     var curTableViewIndexPath = IndexPath(row: 0, section: 0)
-    let tableViewDataSource = ["포요스타", "비요스타", "키요스타", "마요스타", "모두스타"]
-    let starBadgeTypes: [BadgeType] = [.poyoStar, .biyoStar, .kiyoStar, .mayoStar, .allStar]
+    let tableViewDataSource = ["포요스타", "키요스타", "비요스타", "마요스타", "모두스타"]
+    let starBadgeTypes: [BadgeType] = [.poyoStar, .kiyoStar, .biyoStar, .mayoStar, .allStar]
 
     // MARK: - UI Properties
 
+    // 테스트 용 UI
     private lazy var testIncreaseBadge: UIButton = {
         let button = UIButton()
         button.setTitle("뱃지늘리고 실제로 받는 버튼", for: .normal)
@@ -72,6 +78,7 @@ final class BoardViewController: BaseViewController {
         return stackView
     }()
 
+    // 드롭다운 관련
     private lazy var dropdownBackgroundView: UIView = {
         let testView = UIView()
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(removeDropdownBackgroundView))
@@ -97,7 +104,8 @@ final class BoardViewController: BaseViewController {
         return button
     }()
 
-    private lazy var fixedBadgeInformation: UIStackView = {
+    // 상단에 고정되는 뱃지 정보
+    private lazy var upperFixedBadgeInformation: UIStackView = {
         let stackView = UIStackView()
 
         let coinBadge = CommonBadgeInfoComponent(image: ImageLiteral.coin)
@@ -125,6 +133,7 @@ final class BoardViewController: BaseViewController {
         return stackView
     }()
 
+    // 뱃지 지도
     private lazy var badgeCollectionFlowLayout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
@@ -141,12 +150,19 @@ final class BoardViewController: BaseViewController {
 
     private let hideBadgeView = HideBadgeView()
 
+    // 스타 획득 축하 모달
+    private let starCollectCongratsView = StarCollectCongratsViewController()
+
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateTotalBadgeFromCoreData()
         setDelegation()
+        if totalBadgeNumberFromCoreData >= 125 {
+            allStarsCollected = true
+        }
+        refreshBoard(targetIndex: getLatestTableViewIndex())
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -154,8 +170,8 @@ final class BoardViewController: BaseViewController {
     }
 
     override func render() {
-        view.addSubview(fixedBadgeInformation)
-        fixedBadgeInformation.snp.makeConstraints { make in
+        view.addSubview(upperFixedBadgeInformation)
+        upperFixedBadgeInformation.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(BoardSize.upperBadgeInfoHeight)
@@ -163,7 +179,7 @@ final class BoardViewController: BaseViewController {
 
         view.addSubview(badgeCollectionView)
         badgeCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(fixedBadgeInformation.snp.bottom)
+            make.top.equalTo(upperFixedBadgeInformation.snp.bottom)
             make.leading.trailing.bottom.equalToSuperview()
         }
 
@@ -195,8 +211,10 @@ final class BoardViewController: BaseViewController {
         badgeCollectionView.dataSource = self
         dropdownTableView.delegate = self
         dropdownTableView.dataSource = self
+        starCollectCongratsView.delegate = self
     }
 
+    // 드롭다운
     @objc func showDropdown(_ sender: UIButton) {
         addDropdownBackgroundView(buttonFrame: sender.frame)
     }
@@ -205,6 +223,7 @@ final class BoardViewController: BaseViewController {
         dropdownBackgroundView.frame = view.frame
         dropdownTableView.frame = CGRect(
             x: buttonFrame.origin.x + 5,
+            // FIXME: - 여기서 Navigation Height을 적용해야 할 듯
             y: buttonFrame.origin.y + buttonFrame.height + BoardSize.safeAreaTopInset,
             width: BoardSize.tableViewRowWidth,
             height: 0
@@ -239,6 +258,7 @@ final class BoardViewController: BaseViewController {
         }
     }
 
+    // 이전 뱃지 획득 여부에 따른 뷰 전환
     func addHideBadgeView(previous: String?) {
         hideBadgeView.isHidden = false
         hideBadgeView.infoLabel.text = "\(previous ?? "이전 스타")를 획득해야\n시작할 수 있어요!"
@@ -260,22 +280,20 @@ final class BoardViewController: BaseViewController {
         hideBadgeView.alpha = 0
     }
 
-    private func animate(of animations: @escaping () -> Void) {
-        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: animations)
-    }
-
+    // 화면에 뱃지 개수 업데이트
     private func updateBadgeInformation() {
-        let coin = fixedBadgeInformation.arrangedSubviews[1]
+        let coin = upperFixedBadgeInformation.arrangedSubviews[1]
         let coinLabel = coin.subviews.first as? UILabel
         coinLabel?.text = "\(min(totalBadgeNumber, 125))"
-        let shield = fixedBadgeInformation.arrangedSubviews[2]
+        let shield = upperFixedBadgeInformation.arrangedSubviews[2]
         let shieldLabel = shield.subviews.first as? UILabel
         shieldLabel?.text = "\(totalBadgeNumber / 5)"
-        let star = fixedBadgeInformation.arrangedSubviews[3]
+        let star = upperFixedBadgeInformation.arrangedSubviews[3]
         let starLabel = star.subviews.first as? UILabel
         starLabel?.text = "\(totalBadgeNumber / 25)"
     }
 
+    // CoreData에서 전체 뱃지 수 가져오기
     private func updateTotalBadgeFromCoreData() {
         do {
             let getBadgeNumberFromCoreData = try BadgeManager.shared.numberOfTotalCoins()
@@ -287,6 +305,7 @@ final class BoardViewController: BaseViewController {
         }
     }
 
+    // 가장 최근에 수집한 뱃지 위치로 이동
     func scrollCollectionView() {
         let badgeNumberWhenFull = (curTableViewIndexPath.row + 1) * 25
         let badgeNumberWhenEmpty = badgeNumberWhenFull - 25
@@ -302,20 +321,23 @@ final class BoardViewController: BaseViewController {
         }
     }
 
-    @objc private func showCongratsModal() {
-        let congratsModal = StarCollectCongratsViewController()
+    // 스타 화면 보여주기
+    @objc private func showCongratsModal(goToNewPage: Bool) {
+        let congratsModal = starCollectCongratsView
         congratsModal.modalPresentationStyle = .fullScreen
-        congratsModal.badgeType = starBadgeTypes[curTableViewIndexPath.row]
+
+        congratsModal.badgeType = starBadgeTypes[goToNewPage ? max(curTableViewIndexPath.row - 1, 0) : max(curTableViewIndexPath.row, 0)]
         navigationController?.present(congratsModal, animated: true)
     }
 
+    // 특정 스타 뱃지 보드로 이동
     func refreshBoard(targetIndex: Int) {
-        dropdownButton.setTitle("\(tableViewDataSource[targetIndex]) ▼", for: .normal)
+        dropdownButton.setTitle("\(tableViewDataSource[min(targetIndex, 4)]) ▼", for: .normal)
         removeDropdownBackgroundView()
 
         curTableViewIndexPath = IndexPath(row: targetIndex, section: 0)
 
-        let decreaseAmount = targetIndex
+        let decreaseAmount = targetIndex * 25
         totalBadgeNumber = totalBadgeNumberFromCoreData - decreaseAmount
 
         if totalBadgeNumber < 0, targetIndex > 0 {
@@ -329,7 +351,15 @@ final class BoardViewController: BaseViewController {
         scrollCollectionView()
     }
 
+    // 모았던 뱃지를 실제 CoreData에 저장하기
     func recieveTodaysBadges() {
+        var previousTotalBadge = 0
+        do {
+            previousTotalBadge = try BadgeManager.shared.numberOfTotalCoins().result
+        } catch {
+            print("error")
+        }
+
         var updatedTotalBadge = 0
         var todaysBadgeNumber = 0
 
@@ -341,6 +371,8 @@ final class BoardViewController: BaseViewController {
                 let getBadgeNumberFromCoreData = try BadgeManager.shared.numberOfTotalCoins()
                 totalBadgeNumber = getBadgeNumberFromCoreData.result
                 totalBadgeNumberFromCoreData = getBadgeNumberFromCoreData.result
+
+                // 가장 최근 뱃지로 이동
                 updateBadgeInformation()
                 refreshBoard(targetIndex: min((totalBadgeNumberFromCoreData - 1) / 25, 4))
 
@@ -353,25 +385,26 @@ final class BoardViewController: BaseViewController {
             print(error)
         }
 
-        // 제일 최근 스타 단계로 이동
-        var latestStarIndex: Int = (updatedTotalBadge - 1) / 25
-        refreshBoard(targetIndex: min(latestStarIndex, 4))
+        var latestStarIndex = getLatestTableViewIndex()
 
         // TODO: - 인터랙션 보여주기
         showRecievedBadgesInteraction()
 
         // alert 띄우기, Star 뱃지면 모달 띄우기
-        makeAlert(title: "획득한 보상뱃지 총 \(todaysBadgeNumber)개", message: "어제 임무완수의 결과입니다.\n아이와 함께 결과를 보고\n결과에 맞는 칭찬과 응원을 해주세요.", okayAction: { _ in
-            if todaysBadgeNumber == 0 {
-                return
-            }
+        let justGotStarBadge = updatedTotalBadge % 25 == 0
+        let gotStarBadgeAndMore = previousTotalBadge / 25 < latestStarIndex
 
-            if updatedTotalBadge % 25 == 0 {
-                self.showCongratsModal()
-            }
-        })
+        showBadgeCollectedAlert(todaysBadgeNumber: todaysBadgeNumber, didGetStarBadge: justGotStarBadge || gotStarBadgeAndMore)
 
         // TODO: - 임무 완료 메시지도 반영
+    }
+
+    func showBadgeCollectedAlert(todaysBadgeNumber: Int, didGetStarBadge: Bool) {
+        makeAlert(title: "획득한 보상뱃지 총 \(todaysBadgeNumber)개", message: "어제 임무완수의 결과입니다.\n아이와 함께 결과를 보고\n결과에 맞는 칭찬과 응원을 해주세요.", okayAction: { _ in
+            if didGetStarBadge {
+                self.showCongratsModal(goToNewPage: todaysBadgeNumber % 25 != 0 && self.totalBadgeNumberFromCoreData < 125)
+            }
+        })
     }
 
     @objc func testIncreaseBadgeNumber() {
@@ -396,10 +429,27 @@ final class BoardViewController: BaseViewController {
     }
 
     func showRecievedBadgesInteraction() {
+        // TODO: - 아마 다음 스프린트에..? 인터랙션 넣기
         print("showRecievedBadgesInteraction")
     }
 
-    func showHiddenStarMessage() {
-        print("showHiddenStarMessage")
+    func getLatestTableViewIndex() -> Int {
+        return (totalBadgeNumberFromCoreData - 1) / 25
+    }
+
+    // 공통적으로 쓰이는 animate 함수
+    private func animate(of animations: @escaping () -> Void) {
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: animations)
+    }
+}
+
+// MARK: StarCollectCongratsViewDelegate
+
+extension BoardViewController: StarCollectCongratsViewDelegate {
+    func whatToDoAfterStarModal() {
+        if totalBadgeNumberFromCoreData >= 125 {
+            allStarsCollected = true
+            badgeCollectionView.reloadData()
+        }
     }
 }
